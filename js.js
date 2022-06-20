9b04ab03f977fa527a61c8e7017df58209174925
João Paulo de Lima Novaes <94202128+Jplnovaes@users.noreply.github.com>
2022-06-20 20:04:08 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+var peso = document.getElementById('peso');
+var altura = document.getElementById('altura');
+var exibir = document.getElementById('exibir');
+
+
+function ve(){
+
+    var imc = peso.value / (altura.value*altura.value);
+
+    exibir.innerHTML = imc;
+}
